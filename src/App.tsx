b73b143a2517e3a69af858e5a5d075510afe14fa
SaemsCodes <saemscodes@gmail.com>
2025-10-03 09:10:13 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import NotFound from "./pages/NotFound";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsAndConditions from "./pages/TermsAndConditions";
 import VerifySignature from "./pages/VerifySignature";
-import VoterRegistration from "@/components/VoterRegistrationPage";
+import VoterRegistrationPage from "@/components/VoterRegistration";
 
 const queryClient = new QueryClient();
 
