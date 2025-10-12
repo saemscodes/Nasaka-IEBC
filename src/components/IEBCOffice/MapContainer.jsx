8d62d5c442994c8a7d68c5ddf82553017cea1c32
SaemsCodes <saemscodes@gmail.com>
2025-10-12 14:14:48 +0300

    Update MapContainer.jsx

--- src/components/IEBCOffice/MapContainer.jsx
+++ src/components/IEBCOffice/MapContainer.jsx
@@ -1,7 +1,7 @@
+// src/components/IEBCOffice/MapContainer.jsx
 import React, { forwardRef, useEffect, useImperativeHandle, useState, useCallback } from 'react';
-import { MapContainer as LeafletMap, TileLayer, useMap, LayersControl, useMapEvents } from 'react-leaflet';
+import { MapContainer as LeafletMap, TileLayer, useMap, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import L from 'leaflet';
 
 // Fix for default markers
@@ -71,15 +71,16 @@ const MapController = forwardRef(({ center, zoom, onMapReady, onDoubleTap }, ref
   );
 });
 
-const MapContainer = forwardRef(({ 
-  center, 
-  zoom, 
-  children, 
-  className, 
+MapController.displayName = 'MapController';
+
+const MapContainer = forwardRef(({
+  center,
+  zoom,
+  children,
+  className,
   onMapReady,
   onDoubleTap,
-  showLayerControl = true,
-  ...props 
+  ...props
 }, ref) => {
   const defaultCenter = [-1.286389, 36.817223]; // Nairobi
   const defaultZoom = 10;
@@ -91,14 +92,21 @@ const MapContainer = forwardRef(({
   }, [onDoubleTap]);
 
   return (
-    <div className="relative w-full h-full">
+    <div className="map-wrapper">
       <LeafletMap
         center={center || defaultCenter}
         zoom={zoom || defaultZoom}
-        className={`w-full h-full ${className}`}
-        style={{ position: 'relative', zIndex: 'var(--z-index-map-base)' }}
+        className={`w-full h-full ${className || ''}`}
+        style={{ 
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: 'var(--z-map-base)'
+        }}
         zoomControl={true}
-        doubleClickZoom={false} // Disable default double-click zoom for custom double-tap
+        doubleClickZoom={false}
         {...props}
       >
         <TileLayer
@@ -106,31 +114,14 @@ const MapContainer = forwardRef(({
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         
-        {/* Optional Satellite Layer */}
-        {showLayerControl && (
-          <LayersControl position="topright">
-            <LayersControl.BaseLayer checked name="Standard">
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              />
-            </LayersControl.BaseLayer>
-            <LayersControl.BaseLayer name="Satellite">
-              <TileLayer
-                url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-                attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
-              />
-            </LayersControl.BaseLayer>
-          </LayersControl>
-        )}
-        
-        <MapController 
-          ref={ref} 
-          center={center} 
-          zoom={zoom} 
+        <MapController
+          ref={ref}
+          center={center}
+          zoom={zoom}
           onMapReady={onMapReady}
           onDoubleTap={handleDoubleTap}
         />
+        
         {children}
       </LeafletMap>
     </div>
