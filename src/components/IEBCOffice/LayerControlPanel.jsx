98d139afc9fd9b88bc71ae68cce99a8c164aa8f1
SaemsCodes <saemscodes@gmail.com>
2025-10-12 17:03:11 +0300

    Update LayerControlPanel.jsx

--- src/components/IEBCOffice/LayerControlPanel.jsx
+++ src/components/IEBCOffice/LayerControlPanel.jsx
@@ -11,6 +11,7 @@ const LayerControlPanel = ({
   baseMap = 'standard',
   onBaseMapChange
 }) => {
+  // Available layer definitions
   const availableLayers = [
     {
       id: 'iebc-offices',
@@ -21,7 +22,7 @@ const LayerControlPanel = ({
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
         </svg>
       ),
-      color: 'blue'
+      color: 'primary'
     },
     {
       id: 'constituencies',
@@ -60,6 +61,7 @@ const LayerControlPanel = ({
     }
   ];
 
+  // Base map options
   const baseMapOptions = [
     {
       id: 'standard',
