a98fe1c58968c07919fbbda725bd8411d505da51
SaemsCodes <saemscodes@gmail.com>
2025-11-11 00:26:55 +0300

    Create languages.generated.ts

---
+++ src/i18n/languages.generated.ts
@@ -0,0 +1,20 @@
+// AUTO-GENERATED FILE - DO NOT EDIT MANUALLY
+// This file is generated automatically by scripts/generateLanguages.js
+
+export const LANGUAGES = [
+  {
+    "code": "en",
+    "name": "English",
+    "nativeName": "English"
+  },
+  {
+    "code": "ki",
+    "name": "Kikuyu", 
+    "nativeName": "Kikuyu"
+  },
+  {
+    "code": "sw",
+    "name": "Kiswahili",
+    "nativeName": "Kiswahili"
+  }
+];
